bfc313b1b713abb27ee9d85f05460c5318ba30b3
Kevin Cali <20154415+kevincali@users.noreply.github.com>
2023-03-03 17:57:39 +0100

    feat: add fish

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,15 @@
+fish_vi_key_bindings
+set --global fish_greeting
+set --global __fish_git_prompt_show_informative_status
+
+set --global --export EDITOR $HOME/.local/bin/lvim
+set --global --export VISUAL $HOME/.local/bin/lvim
+
+source $HOME/.asdf/asdf.fish
+source $HOME/.asdf/plugins/java/set-java-home.fish
+
+if status is-interactive
+  alias rm="rm -iv"
+  alias zola="flatpak run org.getzola.zola"
+end
+
