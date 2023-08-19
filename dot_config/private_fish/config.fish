6d806b53c3c571685f36cbcf0548fc7484129be0
Kevin Cali <20154415+kevincali@users.noreply.github.com>
2023-08-19 13:48:36 +0200

    refactor: remove unused things

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,9 +2,6 @@ fish_vi_key_bindings
 set --global fish_greeting
 set --global __fish_git_prompt_show_informative_status
 
-
-source $HOME/.asdf/asdf.fish
-source $HOME/.asdf/plugins/java/set-java-home.fish
 set --global --export EDITOR $HOME/.linuxbrew/bin/hx
 set --global --export VISUAL $HOME/.linuxbrew/bin/hx
 
@@ -13,6 +10,5 @@ if status is-interactive
 
   alias rm="rm -iv"
   alias lg="lazygit"
-  alias zola="flatpak run org.getzola.zola"
 end
 
