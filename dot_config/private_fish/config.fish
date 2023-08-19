c7cdf86e98f06cae8a8848b42b3c1cb13fc642d1
Kevin Cali <20154415+kevincali@users.noreply.github.com>
2023-08-19 13:45:31 +0200

    feat: add helix

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,11 +2,11 @@ fish_vi_key_bindings
 set --global fish_greeting
 set --global __fish_git_prompt_show_informative_status
 
-set --global --export EDITOR $HOME/.local/bin/lvim
-set --global --export VISUAL $HOME/.local/bin/lvim
 
 source $HOME/.asdf/asdf.fish
 source $HOME/.asdf/plugins/java/set-java-home.fish
+set --global --export EDITOR $HOME/.linuxbrew/bin/hx
+set --global --export VISUAL $HOME/.linuxbrew/bin/hx
 
 if status is-interactive
   fzf_configure_bindings --directory=\cf # bind dir/file search to Ctrl+F
