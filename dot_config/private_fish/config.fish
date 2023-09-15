758cada4a19be00c184e15c62596ff66fc9fd0ab
Kevin Cali <20154415+kevincali@users.noreply.github.com>
2023-09-15 13:16:34 +0200

    refactor: move envvars

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,9 +2,6 @@ fish_vi_key_bindings
 set --global fish_greeting
 set --global __fish_git_prompt_show_informative_status
 
-set --global --export EDITOR $HOME/.linuxbrew/bin/hx
-set --global --export VISUAL $HOME/.linuxbrew/bin/hx
-
 if status is-interactive
   fzf_configure_bindings --directory=\cf # bind dir/file search to Ctrl+F
 
