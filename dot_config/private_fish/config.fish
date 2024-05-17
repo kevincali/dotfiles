f9e64bf528b69e184770008b9b10fc660f0e35ec
Kevin Cali <20154415+kevincali@users.noreply.github.com>
2024-05-17 16:00:49 +0200

    feat: update fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -7,14 +7,16 @@ set --global fish_greeting
 set --global __fish_git_prompt_show_informative_status
 
 if status is-interactive
-  fzf_configure_bindings --directory=\cf # bind dir/file search to Ctrl+F
+    fzf_configure_bindings --directory=\cf # bind dir/file search to Ctrl+F
 
-  alias rm="rm -iv"
-  alias lg="lazygit"
-  alias ze="zellij --layout code"
+    alias rm="rm -iv"
+    alias lg="lazygit"
+    alias k="kubectl"
+    alias ze="zellij --layout code"
 
-  alias m="go build -ldflags='-linkmode external' cmd/service/main.go && MIRRORD_PROGRESS_MODE=off mirrord exec -f .mirrord/mirrord.yaml ./main"
-  alias md="MIRRORD_PROGRESS_MODE=off mirrord exec -f .mirrord/mirrord.yaml dlv -- debug cmd/service/main.go"
+    alias m="go build -ldflags='-linkmode external' cmd/service/main.go && MIRRORD_PROGRESS_MODE=off mirrord exec -f .mirrord/mirrord.yaml ./main"
+    alias md="MIRRORD_PROGRESS_MODE=off mirrord exec -f .mirrord/mirrord.yaml dlv -- debug cmd/service/main.go"
 end
 
+atuin init fish --disable-up-arrow | source
 zoxide init fish | source
