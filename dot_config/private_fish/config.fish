56ee4c7f1639163a633c4fdbf26ab5b04e7b6d04
Kevin Cali <20154415+kevincali@users.noreply.github.com>
2024-05-19 18:53:01 +0200

    feat: move to CachyOS

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,22 +1,26 @@
-fish_vi_key_bindings
-set --global fish_cursor_insert line
-set --global fish_cursor_visual block
-set --global fish_cursor_replace_one underscore
-
-set --global fish_greeting
-set --global __fish_git_prompt_show_informative_status
+source /usr/share/cachyos-fish-config/cachyos-config.fish
 
 if status is-interactive
-    fzf_configure_bindings --directory=\cf # bind dir/file search to Ctrl+F
-
-    alias rm="rm -iv"
-    alias lg="lazygit"
+    alias hx="helix"
     alias k="kubectl"
+    alias lg="lazygit"
+    alias md="MIRRORD_PROGRESS_MODE=off mirrord exec -f .mirrord/mirrord.yaml dlv -- debug cmd/service/main.go"
+    alias m="go build -ldflags='-linkmode external' cmd/service/main.go && MIRRORD_PROGRESS_MODE=off mirrord exec -f .mirrord/mirrord.yaml ./main"
+    alias rm="rm -iv"
     alias ze="zellij --layout code"
 
-    alias m="go build -ldflags='-linkmode external' cmd/service/main.go && MIRRORD_PROGRESS_MODE=off mirrord exec -f .mirrord/mirrord.yaml ./main"
-    alias md="MIRRORD_PROGRESS_MODE=off mirrord exec -f .mirrord/mirrord.yaml dlv -- debug cmd/service/main.go"
-end
+    function fish_greeting
+    end
 
-atuin init fish --disable-up-arrow | source
-zoxide init fish | source
+    fish_vi_key_bindings
+    set fish_cursor_insert line
+    set fish_cursor_replace_one underscore
+    set fish_cursor_visual block
+    set fish_vi_force_cursor
+
+    fzf_configure_bindings --directory=\cf --variables= # bind dir/file search to Ctrl+F
+    set fzf_fd_opts --hidden --no-ignore
+
+    atuin init fish --disable-up-arrow | source
+    zoxide init fish | source
+end
