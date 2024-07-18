18181d8e0921a45b369e2a0da3e932646f92c126
Kevin Cali <20154415+kevincali@users.noreply.github.com>
2024-07-18 13:57:36 +0200

    feat: update config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,24 +2,34 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 source $XDG_CONFIG_HOME/fish/work.fish
 
 if status is-interactive
+    # aliases
     alias e="zellij --layout code"
     alias hx="helix"
     alias lg="lazygit"
     alias rm="rm -iv"
 
-    function fish_greeting
-    end
+    # custom bindings
+    bind -M insert \ce forward-bigword
+    bind -M insert \cb backward-bigword
+    fzf_configure_bindings --directory=\cf --variables= # bind dir/file search to Ctrl+F
 
+    # use vi bindings
     fish_vi_key_bindings
     set fish_cursor_insert line
     set fish_cursor_replace_one underscore
     set fish_cursor_visual block
     set fish_vi_force_cursor
 
-    fzf_configure_bindings --directory=\cf --variables= # bind dir/file search to Ctrl+F
+    # remove greeting
+    function fish_greeting
+    end
+
+    # options
     set fzf_fd_opts --hidden --no-ignore
 
+    # sourcing
     atuin init fish --disable-up-arrow | source
+    direnv hook fish | source
     zellij setup --generate-completion fish | source
     zoxide init fish | source
 end
