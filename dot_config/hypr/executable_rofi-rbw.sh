86457ca5931855259ff3a47d84bbecc3a7f92492
Kevin Cali <20154415+kevincali@users.noreply.github.com>
2026-01-03 10:52:26 +0100

    feat: move to Omarchy

--- dot_config/hypr/executable_rofi-rbw.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-# This script is used to launch rofi-rbw and if necessary set the rbw pinentry config.
-
-if ! rbw unlocked; then
-    echo "Bitwarden is locked"
-    rbw config set pinentry pinentry-rofi
-fi
-
-rofi-rbw --selector-args "rofi -theme catppuccin-mocha-large"
--- dot_config/wireplumber/wireplumber.conf.d/51-bluez-config.lua
+++
@@ -1,8 +0,0 @@
--- Enable proper codecs for bluetooth headphones.
--- Source: https://wiki.archlinux.org/title/PipeWire#Low_audio_quality_on_Bluetooth
-
-bluez_monitor.properties = {
-  ["bluez5.enable-sbc-xq"] = true,
-  ["bluez5.enable-msbc"] = true,
-  ["bluez5.codecs"] = "[sbc sbc_xq]",
-}
